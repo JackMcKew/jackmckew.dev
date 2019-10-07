4416b14023011a341761253d5f9318b9ed07060a
Jack McKew <jackmckew2@gmail.com>
2019-10-07 13:19:06 +1100

    Image test

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ SITEURL = 'https://nifty-engelbart-ce3324.netlify.com/'
 
 THEME = 'Flex'
 STATIC_PATHS = ['img', 'static']
-FAVICON = 'img/favicon.ico'
+FAVICON = 'img/Jack-McKew-Photo.jpg'
 CUSTOM_CSS = 'static/custom.css'
 
 PATH = 'content'
@@ -25,10 +25,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('All Posts', 'https://jmckew.com/all-posts/'),
+    ('CV/Professional', 'https://jmckew.com/cv-professional/'),
+    ('Contact', 'https://jmckew.com/contact/')
+)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
