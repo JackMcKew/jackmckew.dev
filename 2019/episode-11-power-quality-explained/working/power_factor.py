146a637fc21d078ce73551845dfa1cdbd3ecff63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-26 10:54:00 +0000

    Deploy JackMcKew/jackmckew.dev to github.com/JackMcKew/jackmckew.dev.git:gh-pages

--- 2019/episode-11-power-quality-explained/working/power_factor.py
+++ 2019/episode-11-power-quality-explained/working/power_factor.py
@@ -1,31 +1,31 @@
-from math import sqrt, pi
+from math import sqrt,pi
 
-real_power = 2.2  # Real power in kW
-current = 10  # Current in amps
-voltage = 240  # Voltage in volts
-frequency = 50  # Frequency in hertz
-corrected_pf = 0.95  # Target power factor
+real_power = 2.2     #Real power in kW
+current = 10         #Current in amps
+voltage = 240        #Voltage in volts
+frequency = 50       #Frequency in hertz
+corrected_pf = 0.95  #Target power factor
 
-# Calculate current power factor and apparent power
+#Calculate current power factor and apparent power
 current_pf = 1000 * real_power / (voltage * current)
 S_current = (voltage * current) / 1000
 
-# Power factors greater than 1 will give imaginary Q_current, alert user
+#Power factors greater than 1 will give imaginary Q_current, alert user
 try:
-    # Calculate current reactive power
-    Q_current = sqrt(pow(abs(S_current), 2) - pow(real_power, 2))
+    #Calculate current reactive power
+    Q_current = sqrt(pow(abs(S_current),2) - pow(real_power,2))
 
-    # Calculate target apparent power
+    #Calculate target apparent power
     S_corrected = real_power / corrected_pf
 
-    # Calculate required reactive power compensation
-    Q_corrected = sqrt(pow(S_corrected, 2) - pow(real_power, 2))
+    #Calculate required reactive power compensation
+    Q_corrected = sqrt(pow(S_corrected,2) - pow(real_power,2))
 
-    # Calculate size of capacitor required for reactive power
+    #Calculate size of capacitor required for reactive power
     Q_c = Q_current - Q_corrected
-    C_f = 1000 * Q_c / (2 * pi * frequency * voltage)
+    C_f = 1000 * Q_c / (2*pi*frequency*voltage)
 
-    # Print results to user
+    #Print results to user
     print("Current power factor {0:.3f}".format(current_pf))
     print("Current apparent power {0:.3f} kVA".format(S_current))
     print("Current reactive power {0:.3f} kVAR".format(Q_current))
--- 2020/documentation-with-sphinx/docs/build/html/_sources/generated/sphinxdemo_with_docs.__main__.rst.txt
+++
@@ -1,9 +0,0 @@
-sphinxdemo\_with\_docs.\_\_main\_\_
-===================================
-
-.. rubric:: Description
-.. automodule:: sphinxdemo_with_docs.__main__
-.. currentmodule:: sphinxdemo_with_docs.__main__
-
-
-
