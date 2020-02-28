603b0f31afd74b77b6b613e51e709e0fe7ba6521
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-28 10:14:44 +0000

    Deploy JackMcKew/jackmckew.dev to github.com/JackMcKew/jackmckew.dev.git:gh-pages

--- 2019/hands-on-machine-learning/Chapter 3/working/classification.py
+++ 2019/hands-on-machine-learning/Chapter 3/working/classification.py
@@ -1,13 +1,14 @@
 #%%
 import scipy.io
-mnist = scipy.io.loadmat('scikit_learn_data\\mnist-original.mat')
+
+mnist = scipy.io.loadmat("scikit_learn_data\\mnist-original.mat")
 
 #%%
-X, y = mnist['data'], mnist['label']
+X, y = mnist["data"], mnist["label"]
 X = X.T
 y = y.T
-c,r = y.shape
-y = y.reshape(c,)
+c, r = y.shape
+y = y.reshape(c)
 print(X.shape)
 print(y.shape)
 #%%
@@ -16,88 +17,95 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 some_digit = X[25432]
-some_digit_image = some_digit.reshape(28,28)
+some_digit_image = some_digit.reshape(28, 28)
 
-plt.imshow(some_digit_image,cmap= matplotlib.cm.binary,interpolation='nearest')
-plt.axis('off')
+plt.imshow(some_digit_image, cmap=matplotlib.cm.binary, interpolation="nearest")
+plt.axis("off")
 plt.show
 print(y[25432])
 #%%
-X_train, X_test, y_train, y_test = X[:60000],X[60000:],y[:60000],y[60000:]
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 
 #%%
 import numpy as np
+
 shuffle_index = np.random.permutation(60000)
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
 
 #%%
-y_train_4 = (y_train == 4)
-y_test_4 = (y_test == 4)
-
+y_train_4 = y_train == 4
+y_test_4 = y_test == 4
 
 
 #%%
 from sklearn.linear_model import SGDClassifier
 
 sgd_clf = SGDClassifier(random_state=42)
-sgd_clf.fit(X_train,y_train_4)
+sgd_clf.fit(X_train, y_train_4)
 
 print(sgd_clf.predict([some_digit]))
 
 #%%
 from sklearn.model_selection import cross_val_score
 
-cross_val_score(sgd_clf,X_train,y_train_4,cv=3,scoring='accuracy')
+cross_val_score(sgd_clf, X_train, y_train_4, cv=3, scoring="accuracy")
 
 #%%
 from sklearn.base import BaseEstimator
 
+
 class Never4Classifier(BaseEstimator):
-    def fit(self,X,y=None):
+    def fit(self, X, y=None):
         pass
-    def predict(self,X):
-        return np.zeros((len(X),1),dtype=bool)
+
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+
 
 never_4_clf = Never4Classifier()
-cross_val_score(never_4_clf,X_train,y_train_4,cv=3,scoring='accuracy')
+cross_val_score(never_4_clf, X_train, y_train_4, cv=3, scoring="accuracy")
 
 #%%
 from sklearn.model_selection import cross_val_predict
 
-y_train_pred = cross_val_predict(sgd_clf,X_train,y_train_4,cv=3)
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_4, cv=3)
 
 #%%
 from sklearn.metrics import confusion_matrix
 
-confusion_matrix(y_train_4,y_train_pred)
+confusion_matrix(y_train_4, y_train_pred)
 
 #%%
 from sklearn.metrics import precision_score, recall_score
 
-print(precision_score(y_train_4,y_train_pred))
-print(recall_score(y_train_4,y_train_pred))
+print(precision_score(y_train_4, y_train_pred))
+print(recall_score(y_train_4, y_train_pred))
 
 #%%
 from sklearn.metrics import f1_score
-print(f1_score(y_train_4,y_train_pred))
+
+print(f1_score(y_train_4, y_train_pred))
 
 #%%
 y_scores = sgd_clf.decision_function([some_digit])
 print(y_scores)
 threshold = 0
-y_some_digit_pred = (y_scores > threshold)
+y_some_digit_pred = y_scores > threshold
 print(y_some_digit_pred)
 threshold = 300000
-y_some_digit_pred = (y_scores > threshold)
+y_some_digit_pred = y_scores > threshold
 print(y_some_digit_pred)
 
 #%%
-y_scores = cross_val_predict(sgd_clf, X_train, y_train_4, cv=3,method="decision_function")
+y_scores = cross_val_predict(
+    sgd_clf, X_train, y_train_4, cv=3, method="decision_function"
+)
 
 from sklearn.metrics import precision_recall_curve
 
 precisions, recalls, thresholds = precision_recall_curve(y_train_4, y_scores)
+
 
 def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
@@ -105,6 +113,7 @@ def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     plt.xlabel("Threshold")
     plt.legend(loc="upper left")
     plt.ylim([0, 1])
+
 
 plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
 plt.show()
@@ -114,36 +123,40 @@ from sklearn.metrics import roc_curve
 
 fpr, tpr, thresholds = roc_curve(y_train_4, y_scores)
 
-def plot_roc_curve(fpr,tpr,label=None):
-    plt.plot(fpr,tpr,linewidth=2,label=label)
-    plt.plot([0,1], [0,1], 'k--')
-    plt.axis([0, 1,0,1])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
 
-plot_roc_curve(fpr,tpr)
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], "k--")
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+
+
+plot_roc_curve(fpr, tpr)
 plt.show()
 
 #%%
 from sklearn.metrics import roc_auc_score
 
-print(roc_auc_score(y_train_4,y_scores))
+print(roc_auc_score(y_train_4, y_scores))
 
 #%%
 from sklearn.ensemble import RandomForestClassifier
 
 forest_clf = RandomForestClassifier(random_state=42)
-y_probas_forest = cross_val_predict(forest_clf,X_train,y_train_4,cv=3,method='predict_proba')
-y_scores_forest = y_probas_forest[:,1]
-fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_4,y_scores_forest)
+y_probas_forest = cross_val_predict(
+    forest_clf, X_train, y_train_4, cv=3, method="predict_proba"
+)
+y_scores_forest = y_probas_forest[:, 1]
+fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_4, y_scores_forest)
 
-plt.plot(fpr,tpr,"b:",label="SGD")
-plot_roc_curve(fpr_forest,tpr_forest,"Random Forest")
-plt.legend(loc='bottom right')
+plt.plot(fpr, tpr, "b:", label="SGD")
+plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
+plt.legend(loc="bottom right")
 plt.show()
 
 #%%
-print(roc_auc_score(y_train_4,y_scores_forest))
+print(roc_auc_score(y_train_4, y_scores_forest))
 
 #%%
 sgd_clf.fit(X_train, y_train)
@@ -152,23 +165,23 @@ print(sgd_clf.predict([some_digit]))
 #%%
 from sklearn.neighbors import KNeighborsClassifier
 
-y_train_large = (y_train >= 7)
-y_train_odd = (y_train % 2 == 1)
+y_train_large = y_train >= 7
+y_train_odd = y_train % 2 == 1
 y_multilabel = np.c_[y_train_large, y_train_odd]
 
 knn_clf = KNeighborsClassifier()
-knn_clf.fit(X_train,y_multilabel)
+knn_clf.fit(X_train, y_multilabel)
 
 print(knn_clf.predict([some_digit]))
 
 
 #%%
 y_train_knn_pred = cross_val_predict(knn_clf, X_train, y_train, cv=3)
-print(f1_score(y_train,y_train_knn_pred,average="macro"))
+print(f1_score(y_train, y_train_knn_pred, average="macro"))
 
 #%%
-noise = np.randint(0,100,(len(X_train),784))
-noise = np.randint(0,100,(len(X_test),784))
+noise = np.randint(0, 100, (len(X_train), 784))
+noise = np.randint(0, 100, (len(X_test), 784))
 
 X_train_mod = X_train + noise
 X_test_mod = X_test + noise
--- 2019/intro-to-web-scraping/notebooks/Scraping_Analysis.py
+++ 2019/intro-to-web-scraping/notebooks/Scraping_Analysis.py
@@ -6,17 +6,18 @@ import operator
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+
 #%%
 
-names_df = pd.read_csv('names.csv', header=0, sep=',', quotechar='"')
+names_df = pd.read_csv("names.csv", header=0, sep=",", quotechar='"')
 display(names_df)
 
 #%%
-nz_names = names_df['Place Name'].tolist()
-nz_dict = { i : 0 for i in nz_names }
+nz_names = names_df["Place Name"].tolist()
+nz_dict = {i: 0 for i in nz_names}
 
 #%%
-letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 lcount = dict(OrderedDict([(l, 0) for l in letters]))
 display(lcount)
@@ -30,7 +31,7 @@ for name in nz_names:
         if c.upper() in letters:
             city_dict[c.upper()] += 1
 
-display(nz_dict) 
+display(nz_dict)
 #%%
 total_df = pd.DataFrame.from_dict(nz_dict)
 total_df = total_df.T
@@ -42,17 +43,17 @@ total_df.idxmax()
 max_letters_cities = total_df.idxmax().tolist()
 lettercounts = total_df.loc[total_df.idxmax()].max().tolist()
 maxletters = dict(OrderedDict([(l, 0) for l in letters]))
-for i,l in enumerate(letters):
+for i, l in enumerate(letters):
     maxletters[l] = max_letters_cities[i]
-    maxletters[l] = (lettercounts[i])
+    maxletters[l] = lettercounts[i]
 
 display(maxletters)
 
 #%%
 summary_df = pd.DataFrame()
 scale = 1
-summary_df['Word_Name'] = total_df.idxmax()
-summary_df['Count'] = total_df.loc[total_df.idxmax()].max()
+summary_df["Word_Name"] = total_df.idxmax()
+summary_df["Count"] = total_df.loc[total_df.idxmax()].max()
 # summary_df['Latitude'] = summary_df['City_Name'].map(cities_df.set_index(['AccentCity'])['Latitude'].to_dict()) * scale
 # summary_df['Longitude'] = summary_df['City_Name'].map(cities_df.set_index(['AccentCity'])['Longitude'].to_dict()) * scale
 
@@ -72,14 +73,16 @@ source = ColumnDataSource(summary_df)
 output_file("letter_count.html")
 
 hover = HoverTool()
-hover.tooltips=[
-    ('Word', '@Word')
-]
+hover.tooltips = [("Word", "@Word")]
 
-p = figure(x_range=summary_df.index.tolist(), plot_height=250, title="Letter Counts",
-           toolbar_location=None)
+p = figure(
+    x_range=summary_df.index.tolist(),
+    plot_height=250,
+    title="Letter Counts",
+    toolbar_location=None,
+)
 
-p.vbar(x='index', top='Count', width=0.9,source=source)
+p.vbar(x="index", top="Count", width=0.9, source=source)
 p.add_tools(hover)
 p.xgrid.grid_line_color = None
 p.y_range.start = 0
