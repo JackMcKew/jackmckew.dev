c989a2fdfbb269adc24db14f8b272b4717e95092
Fizban <jackmckew2@gmail.com>
2020-03-13 21:08:39 +1100

    Jupyter notebooks included

--- drafts/2020/markdown-word-counter/main.py
+++ drafts/2020/markdown-word-counter/main.py
@@ -3,9 +3,13 @@ import re
 import sys
 import pathlib
 from typing import List
+import nbformat
+import io
+from nbformat import current
 
-def count_words_in_markdown(markdown: str):
-    text: str = markdown
+def count_words_in_markdown(filePath: str):
+    with open(filePath, 'r', encoding='utf8') as f:
+        text = f.read()
 
     # Comments
     text = re.sub(r'<!--(.*?)-->', '', text, flags=re.MULTILINE)
@@ -34,19 +38,53 @@ def count_words_in_markdown(markdown: str):
 
     return len(text.split())
 
+def count_words_in_jupyter(filePath: str, returnType:str = 'markdown'):
+    with io.open(filePath, 'r', encoding='utf-8') as f:
+        nb = current.read(f, 'json')
+
+    word_count_markdown: int = 0
+    word_count_heading: int = 0
+    word_count_code: int = 0
+    for cell in nb.worksheets[0].cells:
+        if cell.cell_type == "markdown":
+            word_count_markdown += len(cell['source'].replace('#', '').lstrip().split(' '))
+        elif cell.cell_type == "heading":
+            word_count_heading += len(cell['source'].replace('#', '').lstrip().split(' '))
+        elif cell.cell_type == "code":
+            word_count_code += len(cell['input'].replace('#', '').lstrip().split(' '))
+
+    if returnType == 'markdown':
+        return word_count_markdown
+    elif returnType == 'heading':
+        return word_count_heading
+    elif returnType == 'code':
+        return word_count_code
+    else:
+        return Exception
+
+
 
 topFolder: pathlib.Path = pathlib.Path('C:\\Users\\jackm\\Documents\\GitHub\\jackmckew.dev\\content\\')
 
 allMarkdown: List  = []
+allJupyter: List  = []
 
 for singleFile in topFolder.glob('**/*'):
-     if singleFile.suffix == '.md':
-         # Append path        
-         allMarkdown.append(singleFile)
+    if singleFile.suffix == '.md':
+        # Append path        
+        allMarkdown.append(singleFile)
+    if singleFile.suffix == '.ipynb' and not 'checkpoint' in singleFile.name:
+        allJupyter.append(singleFile)
+
+# for singleNotebook in allJupyter:
+#     print(singleNotebook)
+#     print(count_words_in_jupyter(singleNotebook))
 
 totalNumberOfWords: int = 0
 for singlePost in allMarkdown:
-    with open(singlePost, 'r', encoding='utf8') as f:
-            totalNumberOfWords += count_words_in_markdown(f.read())        
+    # with open(singlePost, 'r', encoding='utf8') as f:
+        totalNumberOfWords += count_words_in_markdown(singlePost)
+for singleNotebook in allJupyter:
+    totalNumberOfWords += count_words_in_jupyter(singleNotebook)
 
 print(totalNumberOfWords)
