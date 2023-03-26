b96b71ed8f962c8abc15f12dfaa3eaebab957ddf
Jack McKew <jackmckew2@gmail.com>
2023-03-26 12:57:51 +1100

    Turn on adsense

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,18 +21,18 @@ RELATED_POSTS_SKIP_SAME_CATEGORY = True
 DISQUS_SITENAME = "jackmckew-dev"
 GOOGLE_ANALYTICS = "UA-131173168-2"
 
-# GOOGLE_ADSENSE = {
-#     'ca_id': 'ca-pub-1052275056735196',    # Your AdSense ID
-#     'page_level_ads': True,          # Allow Page Level Ads (mobile)
-#     'ads': {
-#         'aside': '1234561',          # Side bar banner (all pages)
-#         'main_menu': '1234562',      # Banner before main menu (all pages)
-#         'index_top': '1234563',      # Banner after main menu (index only)
-#         'index_bottom': '1234564',   # Banner before footer (index only)
-#         'article_top': '1234565',    # Banner after article title (article only)
-#         'article_bottom': '1234566', # Banner after article content (article only)
-#     }
-# }
+GOOGLE_ADSENSE = {
+    'ca_id': 'ca-pub-1052275056735196',    # Your AdSense ID
+    'page_level_ads': True,          # Allow Page Level Ads (mobile)
+    'ads': {
+        'aside': '1234561',          # Side bar banner (all pages)
+        'main_menu': '1234562',      # Banner before main menu (all pages)
+        'index_top': '1234563',      # Banner after main menu (index only)
+        'index_bottom': '1234564',   # Banner before footer (index only)
+        'article_top': '1234565',    # Banner after article title (article only)
+        'article_bottom': '1234566', # Banner after article content (article only)
+    }
+}
 
 USE_TIPUE_SEARCH = True
 
